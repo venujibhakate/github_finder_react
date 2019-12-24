111965a2d314f95ad5cfbb0a89d7454e7b416d26
Your Name <you@example.com>
2019-12-24 09:25:00 +0530

    get response

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Grid } from '@material-ui/core';
 import SearchBar from './SearchBar';
 
 
 class App extends Component {
-  render(){
-  return (
-    <Grid justify="center" container spacing={16}>
-    <Grid item xs={11}>
-      <Grid container spacing={10}>
-        <Grid item xs={12}>
-          <SearchBar onFormSubmit={this.handleSubmit}/>
-        </Grid>
-      </Grid>
-    </Grid>
-  </Grid>
-  );
+  render() {
+    return (
+      //     <Grid justify="center" container spacing={16}>
+      //     <Grid item xs={11}>
+      //       <Grid container spacing={10}>
+      //         <Grid item xs={12}>
+      //           <SearchBar onFormSubmit={this.handleSubmit}/>
+      //         </Grid>
+      //       </Grid>
+      //     </Grid>
+      //   </Grid>
+      //   );
+      // }
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
-}
-export default App;
+  export default App;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import { Paper, TextField } from '@material-ui/core';
-class SearchBar extends React.Component {
-    state = {
-        SearchTerm: ''
-    }
+import githubUrl from './githubUrl';
 
-    handleChange = (event) => this.setState({ SearchTerm:event.target.value})
+class SearchBar extends React.Component {
+    constructor(props){
+    super(props);
+    this.state = {
+        query: '',
+    }
+    }
+    handleChange = (event) => this.setState({ query:event.target.value})
         // console.log(event.target.value);
     
-    handleSubmit = () => {
-        const {SearchTerm} = this.state;
+    handleSubmit = async event => {
+        // const {query} = this.state;
+        // const {onFormSubmit} = this.props;
+
+        // onFormSubmit(query);
+            event.preventDefault();
+            const response = await githubUrl.get('users', {
+                params: {
+                    q:this.state.query,
+                },
+            });
+            console.log(response.data.items)
+         
     }
+
     
 
     render() {
         return (
+            <div>
             <Paper elevation={6} style={{ padding: '25px' }}>
                 <form onSubmit={this.handleSubmit}>
                     <TextField
@@ -24,6 +41,7 @@ class SearchBar extends React.Component {
                 </form>
 
             </Paper>
+            </div>
         )
     }
 }
