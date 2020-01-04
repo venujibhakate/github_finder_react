50417e429538c943be1d4822e7f6b66511498528
Your Name <you@example.com>
2020-01-04 23:16:48 +0530

    add the view profile button which can show github repo pase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './SearchBar';
+import { Grid } from '@material-ui/core';
 
 
 class App extends Component {
   render() {
     return (
-      //     <Grid justify="center" container spacing={16}>
-      //     <Grid item xs={11}>
-      //       <Grid container spacing={10}>
-      //         <Grid item xs={12}>
-      //           <SearchBar onFormSubmit={this.handleSubmit}/>
-      //         </Grid>
-      //       </Grid>
-      //     </Grid>
-      //   </Grid>
-      //   );
-      // }
-      <div>
-        <SearchBar />
-      </div>
+          <Grid justify="center" container spacing={16}>
+          <Grid item xs={11}>
+            <Grid container spacing={10}>
+              <Grid item xs={12}>
+                <SearchBar/>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
     )
   }
 }
